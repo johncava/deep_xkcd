197d4746d6febba586cee9be2aaaf81a62ba0c3b
johncava <jcava@asu.edu>
2018-05-23 08:34:05 +0000

    batched CNN

--- graph.py
+++ graph.py
@@ -3,10 +3,10 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 
-loss = np.load('loss28.npy')
+loss = np.load('loss30.npy')
 plt.plot(xrange(1,len(loss) + 1), loss)
 plt.xlabel('Iterations')
 plt.ylabel('MSE Loss')
-plt.title('MSE Loss of Mask-R-CNN-2 (6_epoch) lr=1e-3')
+plt.title('MSE Loss of Mask-R-CNN-4 [Batch = 3] (6_epoch) lr=5e-4')
 plt.show()
-plt.savefig('result_loss28.png')
+plt.savefig('result_loss30.png')
