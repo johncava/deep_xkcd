cfee73137869601675759d9fb5074262984466a3
johncava <jcava@asu.edu>
2018-05-25 20:00:04 +0000

    experiment on CNN-LSTM

--- graph.py
+++ graph.py
@@ -3,10 +3,10 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 
-loss = np.load('loss33.npy')
+loss = np.load('loss34.npy')
 plt.plot(xrange(1,len(loss) + 1), loss)
 plt.xlabel('Iterations')
-plt.ylabel('MSE Loss')
-plt.title('MSE Loss of Mask-R-CNN-4 [Batch = 3] (30_epoch) lr=1e-3')
+plt.ylabel('Cross Entropy Loss')
+plt.title('Cross Entropy Loss of CNN-LSTM Model Omega (6_epoch) lr=1e-4')
 plt.show()
-plt.savefig('result_loss33.png')
+plt.savefig('result_loss34.png')
